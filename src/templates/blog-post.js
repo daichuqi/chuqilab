2c160d500ef05e35e380510b6512a6d039aa0a46
Richie Dai <daichuqi@gmail.com>
2017-12-30 23:37:37 -0800

    add swipe

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,23 +5,46 @@ import NextPrevButtons from '../components/NextPrevButtons'
 import '../styles/blog-post.scss'
 import moment from 'moment-timezone'
 import getDateString from '../utils/date-string'
+import Swipe from 'react-easy-swipe'
 
 const Template = ({ data, location, pathContext }) => {
   const { markdownRemark: post } = data
   const { frontmatter: { title, date }, html } = post
   const { prev, next } = pathContext
   const d = getDateString(date)
+  let nav = false;
+  const onSwipeEnd = data => {
+    if (nav === 'next' && next) {
+      window.location = `${next.frontmatter.path}`;
+    } else if (nav === 'prev' && prev) {
+      window.location = `${prev.frontmatter.path}`;
+    }
+  }
+  const onSwipeMove = ({ x }) => {
+    if (x > 200) {
+      nav = 'next'
+    } else if (x < -200) {
+      nav = 'prev';
+    } else {
+      nav = false;
+    }
+  }
   return (
     <div className="blog-post-page template-wrapper">
-      <Helmet title={`${title} - My Blog`} />
-      <div>
-        <div className="blog-detail-header">
-          <div className="title">{title}</div>
-          <div className="date">{d}</div>
+      <Swipe
+        onSwipeEnd={onSwipeEnd}
+        onSwipeMove={onSwipeMove}
+      >
+        <Helmet title={`${title} - My Blog`} />
+        <div>
+          <div className="blog-detail-header">
+            <div className="title">{title}</div>
+            <div className="date">{d}</div>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <NextPrevButtons prev={prev} next={next} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-        <NextPrevButtons prev={prev} next={next} />
-      </div>
+      </Swipe>
     </div>
   )
 }
