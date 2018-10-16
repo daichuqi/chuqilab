073d58c5b38b95dc211e47a900e31e18be2c87ec
Richie Dai <daichuqi@gmail.com>
2018-10-15 23:54:33 -0700

    use react google component and fix style

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -1,45 +1,47 @@
 import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
 
 // import MapStyles from './map-style.json'
 import MapStylesWhite from './map-style-white.json'
 import './style.scss'
 
 const GOOGLE_MAP_API = 'AIzaSyBrUFUbL2fJBsKBjDrdwSepNPpF9t6OZmA'
-const GOOGLE_MAP_URL = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API}`
 
 export default class GoogleMap extends Component {
-  UNSAFE_componentWillMount() {
-    if (typeof window !== 'undefined') {
-      const loader = require('scriptjs')
-      loader(GOOGLE_MAP_URL, () => this.initMap())
-    }
+  static defaultProps = {
+    center: {
+      lat: 37.3875665,
+      lng: -121.99419
+    },
+    zoom: 11
   }
 
-  initMap() {
-    const myhome = { lat: 37.3875665, lng: -121.99419 }
-    const map = new window.google.maps.Map(
-      document && document.getElementById('GoogleMap'),
-      {
-        zoom: 11,
-        center: myhome,
-        styles: MapStylesWhite,
-        zoomControl: false,
-        mapTypeControl: false,
-        scaleControl: false,
-        streetViewControl: false,
-        rotateControl: false,
-        fullscreenControl: false
-      }
-    )
-    new window.google.maps.Marker({
-      position: myhome,
-      icon:
-        'https://s3.amazonaws.com/sneakpeeq-sites/jacklinks/images/marker.png',
-      map
-    })
-  }
+  createMapOptions = maps => ({
+    panControl: false,
+    mapTypeControl: false,
+    scrollwheel: false,
+    styles: MapStylesWhite
+  })
 
   render() {
-    return <div id="GoogleMap" className="google-map-component" />
+    return (
+      <div id="GoogleMap" className="google-map-component">
+        <GoogleMapReact
+          options={this.createMapOptions}
+          bootstrapURLKeys={{ key: GOOGLE_MAP_API }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}>
+          <div lat={this.props.center.lat} lng={this.props.center.lng}>
+            <img
+              alt="my-apt"
+              style={{
+                transform: 'translate(-50%, -50%)'
+              }}
+              src="https://s3.amazonaws.com/sneakpeeq-sites/jacklinks/images/marker.png"
+            />
+          </div>
+        </GoogleMapReact>
+      </div>
+    )
   }
 }
