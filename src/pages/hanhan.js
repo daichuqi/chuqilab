ca814f71e42255c1effaeaf6736eabf3c96416fc
Chuqi Dai <chuqidai@172-13-2-86.lightspeed.mssnks.sbcglobal.net>
2019-06-16 19:41:43 +0200

    fix build

--- src/pages/hanhan.js
+++ src/pages/hanhan.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Baroque from '../projects/baroque/js/main'
+import Baroque from '../projects/baroque/js'
 
 export default class Hanhan extends Component {
   render() {
