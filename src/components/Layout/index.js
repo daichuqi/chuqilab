9c1af5995fe489bdfaa6b3c509fc93e4debc90f3
Chuqi Dai <chuqidai@172-13-2-86.lightspeed.mssnks.sbcglobal.net>
2019-06-16 13:53:10 +0200

    fix build

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import Navbar from './Navbar'
 
 import 'antd/dist/antd.css'
-import '../styles/libs/prism-darcula.css'
-import '../styles/default.scss'
-import '../styles/responsive.scss'
-import '../styles/style.scss'
-import '../styles/birthday.scss'
+import '../../styles/libs/prism-darcula.css'
+import '../../styles/default.scss'
+import '../../styles/responsive.scss'
+import '../../styles/style.scss'
+import '../../styles/birthday.scss'
 
 export default class Layout extends Component {
   render() {
