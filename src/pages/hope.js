0ac0060df80c55c5163c2410fef2479d643ecc6c
daichuqi <daichuqi@gmail.com>
2019-06-29 14:19:43 +0200

    fix build

--- src/pages/hope.js
+++ src/pages/hope.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Modal } from 'antd'
 
+import Layout from '../components/Layout'
 import hope from '../assets/hope.jpg'
 import airpod from '../assets/airpod.png'
 
@@ -11,99 +12,101 @@ export default class Hope extends Component {
   }
   render() {
     return (
-      <div
-        style={{
-          backgroundColor: 'black',
-          height: '100vh',
-          border: '30px solid #ff2c2c',
-        }}
-      >
+      <Layout hide>
         <div
           style={{
-            textAlign: 'center',
+            backgroundColor: 'black',
+            height: '100vh',
+            border: '30px solid #ff2c2c',
           }}
         >
-          <img
+          <div
             style={{
-              width: 400,
-              height: 400,
-              filter: 'contrast(100)',
+              textAlign: 'center',
             }}
-            src={hope}
-            alt="hope"
-          ></img>
+          >
+            <img
+              style={{
+                width: 400,
+                height: 400,
+                filter: 'contrast(100)',
+              }}
+              src={hope}
+              alt="hope"
+            ></img>
+
+            <div
+              style={{
+                fontSize: 50,
+              }}
+            >
+              🎊
+            </div>
+          </div>
 
           <div
             style={{
-              fontSize: 50,
+              marginTop: 30,
+              fontSize: 24,
+              textAlign: 'center',
+              color: '#ccc',
             }}
           >
-            🎊
+            <div>恭喜程小晗同学成功晋级D2</div>
+            <div>特此表扬!</div>
+
+            <Button
+              type="primary"
+              onClick={() => {
+                if (this.state.n === 4) {
+                  this.setState({ loading: true })
+                  setTimeout(() => {
+                    Modal.success({
+                      className: 'airpod-modal',
+                      title: 'AirPod特别定制版',
+                      okText: 'I Love it!',
+                      onOk: () => {
+                        Modal.success({
+                          className: 'airpod-modal',
+                          title: '',
+                          okText: <span>LOL</span>,
+                          onOk: () => {},
+                          content: (
+                            <div>
+                              <img
+                                style={{
+                                  width: '100%',
+                                }}
+                                src="https://chuqi-gatsby.s3-us-west-1.amazonaws.com/629A6A25-ACA0-4343-BCB5-905CD7BC5FD5.GIF"
+                              ></img>
+                            </div>
+                          ),
+                        })
+                      },
+                      content: (
+                        <div>
+                          <img src={airpod}></img>
+                        </div>
+                      ),
+                    })
+
+                    this.setState({ loading: false, n: 0 })
+                  }, 1000)
+                }
+                this.setState({ n: this.state.n + 1 })
+              }}
+              loading={this.state.loading}
+              size="large"
+              style={{
+                marginTop: 40,
+                transition: 'all 200ms',
+              }}
+            >
+              {text[this.state.n]}
+            </Button>
           </div>
         </div>
-
-        <div
-          style={{
-            marginTop: 30,
-            fontSize: 24,
-            textAlign: 'center',
-            color: '#ccc',
-          }}
-        >
-          <div>恭喜程小晗同学成功晋级D2</div>
-          <div>特此表扬!</div>
-
-          <Button
-            type="primary"
-            onClick={() => {
-              if (this.state.n === 4) {
-                this.setState({ loading: true })
-                setTimeout(() => {
-                  Modal.success({
-                    className: 'airpod-modal',
-                    title: 'AirPod特别定制版',
-                    okText: 'I Love it!',
-                    onOk: () => {
-                      Modal.success({
-                        className: 'airpod-modal',
-                        title: '',
-                        okText: <span>LOL</span>,
-                        onOk: () => {},
-                        content: (
-                          <div>
-                            <img
-                              style={{
-                                width: '100%',
-                              }}
-                              src="https://chuqi-gatsby.s3-us-west-1.amazonaws.com/629A6A25-ACA0-4343-BCB5-905CD7BC5FD5.GIF"
-                            ></img>
-                          </div>
-                        ),
-                      })
-                    },
-                    content: (
-                      <div>
-                        <img src={airpod}></img>
-                      </div>
-                    ),
-                  })
-
-                  this.setState({ loading: false, n: 0 })
-                }, 1000)
-              }
-              this.setState({ n: this.state.n + 1 })
-            }}
-            loading={this.state.loading}
-            size="large"
-            style={{
-              marginTop: 40,
-              transition: 'all 200ms',
-            }}
-          >
-            {text[this.state.n]}
-          </Button>
-        </div>
-      </div>
+      </Layout>
     )
   }
 }
