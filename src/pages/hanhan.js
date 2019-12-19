28df0b4a9f6a6b8d1217ab1b8482c78f84bb8180
Richie <daichuqi@gmail.com>
2019-12-18 22:24:02 -0800

    add back music page

--- src/pages/hanhan.js
+++ src/pages/hanhan.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 
-import Baroque from '../projects/baroque/js'
+// import Baroque from '../projects/baroque/js'
 import Layout from '../components/Layout'
+import Baroque from '../old/projects/baroque/js' 
 
 export default class Hanhan extends Component {
   render() {
