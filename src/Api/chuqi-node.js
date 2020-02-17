6117656abd826e2483559d680e280cce0970f998
daichuqi <daichuqi@gmail.com>
2020-02-16 23:00:43 -0800

    update

--- src/Api/chuqi-node.js
+++ src/Api/chuqi-node.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-const BASE_URL = 'https://chengyuhan.me/api'
+const BASE_URL = 'https://chengyuhan.me'
 
 export const fetchOrCreateUser = async username => {
   const res = await Axios.get(`${BASE_URL}/users/${username}`)
